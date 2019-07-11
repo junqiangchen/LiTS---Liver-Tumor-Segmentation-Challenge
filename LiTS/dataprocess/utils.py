9f96580c29b471871b077ce1122a8a273b8691aa
junqiangchen <1207173174@qq.com>
2019-07-11 14:48:53 +0800

    Update utils.py

--- LiTS/dataprocess/utils.py
+++ LiTS/dataprocess/utils.py
@@ -15,7 +15,7 @@ def file_name_path(file_dir):
 
 def save_file2csv(file_dir, file_name):
     """
-    save file path to csv，
+    save file path to csv,this is for segmentation
     :param file_dir:preprocess data path
     :param file_name:output csv name
     :return:
